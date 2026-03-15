ace54a075894176a8f71e501f36752b74a20558b
harwad <125148176+nervsys@users.noreply.github.com>
2026-03-15 19:15:08 -0300

    Add goroutine worker model

--- checker/services.go
+++ checker/services.go
@@ -4,60 +4,73 @@ import (
 	"fmt"
 	"log"
 	"sniping/helpers"
+	"sync"
 )
 
 func Discord(usernames []string, prefs helpers.Network) {
 	api := helpers.Endpoint.Discord
 	ExceptList := &ExceptList{}
 
+	sem := make(chan struct{}, prefs.Threads)
+	var wg sync.WaitGroup
+
 	for _, username := range usernames {
-		fmt.Printf("Checking Discord for username: %s\n", username)
+		u := username
+		wg.Add(1)
+		sem <- struct{}{}
+		go func() {
+			defer wg.Done()
+			defer func(){ <-sem }()
 
-		// create payload
-		payload := CreateReqBody(username)
+			fmt.Printf("Checking Discord for username: %s\n", u)
 
-		// create client
-		client, err := createCustomClient(helpers.GetProxy(), prefs.Timeout, prefs.Gateway)
-		if err != nil {
-			fmt.Printf("Request failed: Adding %s to exception list...\n", username)
-			HandleException(username, ExceptList)
-			continue
-		}
+			// create payload
+			payload := CreateReqBody(u)
 
-		// make request
-		resp, err := DoRequest(client, "POST", api, payload)
-		if err != nil {
-			fmt.Printf("Request failed: Adding %s to exception list...\n", username)
-			HandleException(username, ExceptList)
-			continue
-		}
-
-		defer resp.Body.Close()
-
-		if resp.StatusCode == 200 {
-			msg, err := GetResponse(resp)
+			// create client
+			client, err := createCustomClient(helpers.GetProxy(), prefs.Timeout, prefs.Gateway)
 			if err != nil {
-    			log.Fatal(err)
+				fmt.Printf("Request failed: Adding %s to exception list...\n", u)
+				HandleException(u, ExceptList)
+				return
 			}
 
-			if (msg["taken"] != true) {
-				fmt.Printf("Username %s is available on Discord\n", username)
-				helpers.SaveChecked(username, true)
-				continue
+			// make request
+			resp, err := DoRequest(client, "POST", api, payload)
+			if err != nil {
+				fmt.Printf("Request failed: Adding %s to exception list...\n", u)
+				HandleException(u, ExceptList)
+				return
 			}
-		}
+			defer resp.Body.Close()
 
-		if resp.StatusCode == 429 {
-			fmt.Printf("Rate limited! Adding %s to exception list...\n", username)
-			HandleException(username, ExceptList)
-			continue
-		}
+			if resp.StatusCode == 200 {
+				msg, err := GetResponse(resp)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if msg["taken"] != true {
+					fmt.Printf("Username %s is available on Discord\n", u)
+					helpers.SaveChecked(u, true)
+					return
+				}
+			}
 
-		fmt.Printf("Username %s is taken on Discord\n", username)
-		helpers.SaveChecked(username, false)
+			if resp.StatusCode == 429 {
+				fmt.Printf("Rate limited! Adding %s to exception list...\n", u)
+				HandleException(u, ExceptList)
+				return
+			}
+
+			fmt.Printf("Username %s is taken on Discord\n", u)
+			helpers.SaveChecked(u, false)
+		}()
 	}
+
+	wg.Wait()
+
 	if len(ExceptList.Users) > 0 { // prevent overflow
 		fmt.Printf("\nProcessing %d usernames in the exception list...\n", len(ExceptList.Users))
-    	ExecExceptionList(ExceptList, prefs)
+		ExecExceptionList(ExceptList, prefs)
 	}
 }
